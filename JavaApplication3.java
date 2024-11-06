ddec85ebc6c1aa59f5acb00777b2aa4154e7265b
Samwt28 <canttouchthisx.onlineclass@gmail.com>
2024-11-06 09:43:35 +0800

    Add files via upload

---
+++ JavaApplication3.java
@@ -0,0 +1,44 @@
+
+package javaapplication3;
+import java.util.Scanner;
+
+public class JavaApplication3 {
+
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter Your First Name: ");
+        String firstname = scanner.nextLine();
+        
+        System.out.print("Enter Your Last Name: ");
+        String lastname = scanner.nextLine();
+        
+        System.out.print("Enter Your Age: ");
+        String age = scanner.nextLine();
+        
+        System.out.print("Enter School ID: ");
+        String id = scanner.nextLine();
+        
+        System.out.print("Enter The Program Your Enrolling In: ");
+        String course = scanner.nextLine();
+        
+        System.out.print("Enter Your Intended Major or Concentration: ");
+        String major = scanner.nextLine();
+        
+        System.out.print("Enter Your GPA: ");
+        String gpa = scanner.nextLine();
+        
+        System.out.print("Enter Your Adress: ");
+        String adress = scanner.nextLine();
+        
+        System.out.print("Enter Guardian or Emergency Contact Full Name: ");
+        String guardianname = scanner.nextLine();
+        
+        System.out.print("Enter Your Adress: ");
+        String contact = scanner.nextLine();
+        
+        
+    }
+    
+}
